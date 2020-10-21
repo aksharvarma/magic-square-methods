eeaa60796a87893fa7759fa124beb8e5c1023514
Akshar Varma <aksharvarma@gmail.com>
2020-10-21 11:45:55 -0400

    Drastically improve info printed by the verify function in utilities

--- utilities.py
+++ utilities.py
@@ -33,29 +33,61 @@ def verify_magic_square(magic_s, print_message=True, print_verbose=False):
     assert n==magic_s.shape[1], "Input is not a square"
     required_sum = calculate_required_sum(n)
     print_message = print_message or print_verbose
+    magic_flag = True           # boolean keeping track of magicness
     if print_message:
-        print("\nStarting to verify if the required sum of {:d} is attained.".format(required_sum))
+        print("\n"+"-"*40)
+        print("Starting verification:\nSquare size: {:d}x{:d}\nRequired sum: {:d}".format(n, n, required_sum))
+        print("-"*20)
+
     # columns
     cols = np.sum(magic_s, 0)
-    assert np.all(cols == required_sum)
-    if print_verbose:
+    # Doesn't add up!
+    if np.any(cols != required_sum):
+        magic_flag = False
+        print("{:d} columns don't add up to required sum".format(np.count_nonzero(cols != required_sum)))
+        if print_verbose:
+            for col in np.argwhere(cols != required_sum).flatten():
+                faulty_column = magic_s[:, col].flatten()
+                print("Column {}: {} adds upto {:d}".format(col+1, faulty_column, np.sum(faulty_column)))
+    elif print_verbose:
         print("Yes. All columns add up to {:d}".format(required_sum))
+
     # rows
     rows = np.sum(magic_s, 1)
-    assert np.all(rows == required_sum)
-    if print_verbose:
+    if np.any(rows != required_sum):
+        magic_flag = False
+        print("{:d} rows don't add up to required sum".format(np.count_nonzero(rows != required_sum)))
+        if print_verbose:
+            for row in np.argwhere(rows != required_sum).flatten():
+                faulty_row = magic_s[row, :].flatten()
+                print("Column {}: {} adds upto {:d}".format(row+1, faulty_row, np.sum(faulty_row)))
+    elif print_verbose:
         print("Yes. All rows add up to {:d}".format(required_sum))
+
     # main diagonal (top left to bottom right)
-    diagonal_1 = np.trace(magic_s)
-    assert diagonal_1 == required_sum
-    if print_verbose:
+    diagonal_1_sum = np.trace(magic_s)
+    if diagonal_1_sum != required_sum:
+        magic_flag = False
+        faulty_diagonal = magic_s[np.arange(n), np.arange(n)]
+        print("The main diagonal: {} adds upto {:d}".format(faulty_diagonal, diagonal_1_sum))
+    elif print_verbose:
         print("Yes. The main diagonal adds up to {:d}".format(required_sum))
+
     # other diagonal (bottom left to top right)
-    diagonal_2 = np.trace(np.rot90(magic_s))
-    assert diagonal_2 == required_sum
-    if print_verbose:
+    diagonal_2_sum = np.trace(np.rot90(magic_s))
+    if diagonal_2_sum != required_sum:
+        magic_flag = False
+        faulty_diagonal = magic_s[np.arange(n)[::-1], np.arange(n)]
+        print("The other diagonal {} adds upto {:d}".format(faulty_diagonal, diagonal_2_sum))
+    elif print_verbose:
         print("Yes. The other diagonal also adds up to {:d}".format(required_sum))
-    # assert cols == rows == diagonal_1 == diagonal_2 == required_sum
+
+    # Final printing if all work out
     if print_message:
-        print("Done. All rows, columns and diagonals add up to {:d}.\nSquare is indeed magic.".format(required_sum))
-    return True
+        if magic_flag:
+            print("Finished checking. All rows, columns and diagonals add up to {:d}.\nSquare is indeed magic!".format(required_sum))
+        else:
+            print("Finished checking. Some rows/columns/diagonals do NOT add up to {:d}.\nSquare is NOT magic!".format(required_sum))
+        print("-"*40)
+
+    return magic_flag
